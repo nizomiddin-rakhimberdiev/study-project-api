3da8c64f7e5de3b0a44018b3dcd955eea427f7f3
nizomiddin-rakhimberdiev <nizomiddinoff@mail.ru>
2023-11-02 00:37:44 +0500

    final commit

--- studyPlatform/urls.py
+++ studyPlatform/urls.py
@@ -25,9 +25,9 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Instagram clone API",
+        title="Study Platform API",
         default_version='v1',
-        description="This apis for instagram clone",
+        description="This apis for Study Platform",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="ranizomiddin@gmail.com"),
         license=openapi.License(name="BSD License"),
