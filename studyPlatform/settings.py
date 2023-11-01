657bae140a8ec66f26b8febd66ce09425b929375
nizomiddin-rakhimberdiev <nizomiddinoff@mail.ru>
2023-11-02 00:00:28 +0500

    nimadir

--- studyPlatform/settings.py
+++ studyPlatform/settings.py
@@ -134,8 +134,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
